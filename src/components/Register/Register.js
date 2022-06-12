b0771cc4c82f342a6604c123c2dc82b569a6842c
ElizabethChillcce <eli.chillcce.08@gmail.com>
2022-06-12 18:15:53 -0500

    Creando formulario de registro y dando estilo con CSS

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -6,26 +6,28 @@ export function Register () {
     //aqui tengo fun
 
         return <div>
+            <section>
                 <section><h2 className="titleMain">YUYARINAPAQ</h2></section>
                 <section className="textContainer">
                     <h3 className="textRegister">Registro</h3>
                     <form>
                     <label for='nameUser'>Nombre de usuario<br></br>
-                    <input type='text' id='createName' placeholder ='Ejm: usuario' name ='nameUser'></input>
+                    <input type='text' id='createName' placeholder ='Ejm: Eli Chil' name ='nameUser'></input>
                     </label><br></br>
                     <label for='inputEmail'>Email<br></br>
-                    <input type='text' id='createEmail' placeholder ='usuario@example.com' name ='nameEmail'></input>
+                    <input type='text' id='createEmail' placeholder ='elichil@example.com' name ='nameEmail'></input>
                     </label>
                     <p id='messageEmail'></p><br></br>
                     <label for='namePassword'>Contraseña<br></br>
                     <input type= 'password' id='createPassword' placeholder ='Mayor a 6 carácteres' name ='namePassword'></input>
                     </label>
                     </form>
-                    <p id='messagePassword'></p>
+                    {/* <p id='messagePassword'></p> */}
                     <p id='messageVerificado'></p>
                     <button id='buttonUserRegister'> Crear Ususario</button>
                     <button id='buttonGoogle'> Iniciar con Google</button>
                     <button id='buttonBackHome'></button>
                 </section>
+            </section>
             </div>;
 }
