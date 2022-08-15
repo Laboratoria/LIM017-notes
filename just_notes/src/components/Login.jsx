bb2d073087761694df730813fc668c2d7d1a14d2
Juana Llanje <juana.lc@hotmail.es>
2022-08-14 23:48:57 -0500

    Adding Gmail and Facebook logos in the Login component

--- just_notes/src/components/Login.jsx
+++ just_notes/src/components/Login.jsx
@@ -3,13 +3,24 @@ import './Login.css';
 export function Login () {
     return (
         <div className='container'>
-        <div className='container-elements'>
+        <div className='containerElements'>
         <img className='img' src={require('../img/logo.png')}/>
         <article className='article'>
-        <div className='information'>
+        <div className='infoEmail'>
         <label>Correo electrónico</label><input type='text' id='email' className='input' placeholder='Ingresa email'/><br/>
+        </div>
+        <div className='infoPassword'>
         <label>Contraseña</label><input type='text' id='password' className='input' placeholder='Ingresa clave'/><br/>
+        </div>
         <button className='btnLogin'>Ingresar</button>
+        <div className='infoForgotOrRegisterEmail'>
+        ¿Te olvidaste de tu cuenta? Ingresa <a href='#'>aquí</a><br/>
+        ¿Aún no te registras? Házlo <a href='#'>aquí</a><br/>
+        O conéctate con:<br/>
+        <div className='containerLogosGmailFacebook'>
+        <img className='img' src={require('../img/gmail.png')}/>
+        <img className='img' src={require('../img/facebook.png')}/>
+        </div>
         </div>
         </article>
         </div>
