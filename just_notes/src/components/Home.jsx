cbda5fe1148c44f1d3b9d4ad0e88bc559228aa3e
Juana Llanje <juana.lc@hotmail.es>
2022-08-14 22:48:41 -0500

    Design Login component 50%

--- just_notes/src/components/Home.jsx
+++ just_notes/src/components/Home.jsx
@@ -3,7 +3,9 @@ import './Login.css';
 export function Home () {
     return (
         <div className="container">
+        <article className="article">
 
+        </article>
         </div>
     )
 }
--- just_notes/src/components/Login.jsx
+++ just_notes/src/components/Login.jsx
@@ -2,8 +2,17 @@ import './Login.css';
 
 export function Login () {
     return (
-        <div className="container">
-
+        <div className='container'>
+        <div className='container-elements'>
+        <img className='img' src={require('../img/logo.png')}/>
+        <article className='article'>
+        <div className='information'>
+        <label>Correo electrónico</label><input type='text' id='email' className='input' placeholder='Ingresa email'/><br/>
+        <label>Contraseña</label><input type='text' id='password' className='input' placeholder='Ingresa clave'/><br/>
+        <button className='btnLogin'>Ingresar</button>
+        </div>
+        </article>
+        </div>
         </div>
     )
 }
