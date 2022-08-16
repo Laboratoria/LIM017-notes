95228f6eef4b0f9b2cd01a2a6ba38d784c70f990
Juana Llanje <juana.lc@hotmail.es>
2022-08-15 23:50:46 -0500

    Design the Home component

--- just_notes/src/components/Home.jsx
+++ just_notes/src/components/Home.jsx
@@ -1,11 +1,17 @@
-import './Login.css';
+import styles from './Home.module.css';
 
 export function Home () {
     return (
-        <div className="container">
-        <article className="article">
-
+        <div className={styles.container}>
+        <div className={styles.containerElements}>
+        <img className={styles.img} src={require('../img/logo.png')}/>
+        <article className={styles.article}>
+        ¿Olvidaste alguna fecha importante? <br/>
+        No te preocupes que esta aplicación te ayudará <br/>
+        a guardar tus eventos o citas más importantes. <br/>
+        <button className={styles.btnEnter}>Ingresa a tu cuenta</button>
         </article>
         </div>
+        </div>
     )
-}
+    }
--- just_notes/src/components/Login.jsx
+++ just_notes/src/components/Login.jsx
@@ -1,25 +1,25 @@
-import './Login.css';
+import styles from './Login.module.css';
 
 export function Login () {
     return (
-        <div className='container'>
-        <div className='containerElements'>
-        <img className='img' src={require('../img/logo.png')}/>
-        <article className='article'>
-        <div className='infoEmail'>
-        <label>Correo electrónico</label><input type='text' id='email' className='input' placeholder='Ingresa email'/><br/>
+        <div className={styles.container}>
+        <div className={styles.containerElements}>
+        <img className={styles.img} src={require('../img/logo.png')}/>
+        <article className={styles.article}>
+        <div className={styles.infoEmail}>
+        <label>Correo electrónico</label><input type='text' id='email' className={styles.input} placeholder='Ingresa email'/><br/>
         </div>
         <div className='infoPassword'>
-        <label>Contraseña</label><input type='text' id='password' className='input' placeholder='Ingresa clave'/><br/>
+        <label>Contraseña</label><input type='text' id='password' className={styles.input} placeholder='Ingresa clave'/><br/>
         </div>
-        <button className='btnLogin'>Ingresar</button>
-        <div className='infoForgotOrRegisterEmail'>
+        <button className={styles.btnLogin}>Ingresar</button>
+        <div className={styles.infoForgotOrRegisterEmail}>
         ¿Te olvidaste de tu cuenta? <a href='#'> Ingresa aquí</a><br/>
         ¿Aún no te registras? <a href='#'> Házlo aquí</a><br/>
         O conéctate con:<br/>
-        <div className='containerLogosGmailFacebook'>
-        <img className='img' src={require('../img/gmail.png')}/>
-        <img className='img' src={require('../img/facebook.png')}/>
+        <div className={styles.containerLogosGmailFacebook}>
+        <img className={styles.img} src={require('../img/gmail.png')}/>
+        <img className={styles.img} src={require('../img/facebook.png')}/>
         </div>
         </div>
         </article>
