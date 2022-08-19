3fb7918627163da0e1d5e337f594708f880e0003
Juana Llanje <juana.lc@hotmail.es>
2022-08-18 23:08:18 -0500

    Adding functionality in the Home component

--- just_notes/src/components/Home.jsx
+++ just_notes/src/components/Home.jsx
@@ -1,14 +1,18 @@
+import { useNavigate } from 'react-router-dom';
 import styles from './Home.module.css';
 
 export function Home () {
+    const navigate = useNavigate();
+    const login = () => {
+        navigate('/login')
+    }
     return (
         <section className={styles.container}>
         <article className={styles.containerElements}>
         <img className={styles.img} src={require('../img/logo.png')}/> <br />
         ¿Olvidaste alguna fecha importante? <br/>
         No te preocupes que esta aplicación <br /> te ayudará a guardar tus eventos o <br /> citas más importantes. <br/>
-        <button className={styles.btnEnter}>Ingresa a tu cuenta</button>
-
+        <button className={styles.btnEnter} onClick={login}>Ingresa a tu cuenta</button>
         </article>
         </section>
     )
